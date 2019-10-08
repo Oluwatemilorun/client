343a81ef9fe4719d0f32a96b074e1c90824548df
Harphies1 <tafeez.tech@gmail.com>
2019-10-08 19:09:46 +0100

    connect dasboard to store

--- src/components/dashboard/home.js
+++ src/components/dashboard/home.js
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
-import axios from "axios";
+import { connect } from "react-redux";
+import { PropTypes } from "prop-types";
+import { getDevices } from "../../store/actions/device";
 
 class home extends Component {
-  state = {
-    devices: null
-  };
-  componentDidMount() {
-    axios
-      .get("/devices")
-      .then(res => {
-        console.log(res.data);
-        this.setState({
-          devices: res.data
-        });
-      })
-      .catch(err => console.log(err));
+  componentWillMount() {
+    this.props.getDevices();
   }
   render() {
+    console.log(this.props.devices);
     return (
       <div>
         <h1>Home Page</h1>
@@ -25,4 +17,18 @@ class home extends Component {
   }
 }
 
-export default home;
+//set the propTypes
+// eslint-disable-next-line react/no-typos
+home.PropTypes = {
+  getDevices: PropTypes.func.isRequired,
+  devices: PropTypes.array.isRequired
+};
+
+const mapStateToProps = state => ({
+  devices: state.device.devices //state.reducername from rootreducer. what type of returm from that reducer
+});
+
+export default connect(
+  mapStateToProps,
+  { getDevices }
+)(home);
