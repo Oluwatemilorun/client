ea45726ad0823224d72e1e74253c91770c115a00
Harphies1 <tafeez.tech@gmail.com>
2019-10-08 17:27:50 +0100

    auth actions

---
+++ src/store/actions/auth.js
@@ -0,0 +1,71 @@
+import {
+  SET_USER,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  SET_UNAUTHENTICATED,
+  LOADING_USER
+} from "./types";
+
+import axios from "axios";
+// Login ction
+export const loginUser = (userData, history) => dispatch => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post("/login", userData)
+    .then(res => {
+      // eslint-disable-next-line no-undef
+      setAuthorizationHeader(res.data.token);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push("/");
+    })
+    .catch(err => {
+      dispatch({ type: SET_ERRORS, payload: err.response.data });
+    });
+};
+
+///get userData
+export const getUserData = () => dispatch => {
+  dispatch({ type: LOADING_USER });
+  axios
+    .get("/user")
+    .then(res => {
+      dispatch({
+        type: SET_USER,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+//logout User Action
+export const logoutUser = () => dispatch => {
+  localStorage.removeItem("FBIdToken");
+  delete axios.defaults.headers.common["Authorization"];
+  dispatch({
+    type: SET_UNAUTHENTICATED
+  });
+};
+
+//signUpUser action
+export const signUpUser = (newUserData, history) => dispatch => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post("/signup", newUserData)
+    .then(res => {
+      // eslint-disable-next-line no-undef
+      setAuthorizationHeader(res.data.token);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push("/");
+    })
+    .catch(err => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
