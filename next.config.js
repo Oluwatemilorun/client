7a64960bb69ace28cb14c661c6d20db13acdb3ea
Isaac Oluwatemilorun <akiolisa@gmail.com>
2019-10-26 03:41:04 +0100

    ish....

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 const withImages = require('next-images');
+const withSass = require('@zeit/next-sass');
 
-module.exports = withImages()
+module.exports = withImages(withSass({
+    cssModules: true,
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /\.mjs$/,
+            include: /node_modules/,
+            type: "javascript/auto"
+        });
+
+        return config;
+    }
+}));
