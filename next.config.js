e49d8ad4558c39e550dd34fb6265da53259a848e
Isaac Oluwatemilorun <akiolisa@gmail.com>
2019-10-27 23:58:25 +0100

    css support + icon

--- next.config.js
+++ next.config.js
@@ -1,15 +1,20 @@
 const withImages = require('next-images');
 const withSass = require('@zeit/next-sass');
+const withCss = require('@zeit/next-css')
 
-module.exports = withImages(withSass({
-    cssModules: true,
-    webpack(config, options) {
-        config.module.rules.push({
-            test: /\.mjs$/,
-            include: /node_modules/,
-            type: "javascript/auto"
-        });
+module.exports = withImages(
+    withCss(
+        withSass({
+            cssModules: true,
+            webpack(config, options) {
+                config.module.rules.push({
+                    test: /\.mjs$/,
+                    include: /node_modules/,
+                    type: "javascript/auto"
+                });
 
-        return config;
-    }
-}));
+                return config;
+            }
+        })
+    )
+);
